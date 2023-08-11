8d4c1fcaf635f6c4ba95161bd4ed0af65668d15a
Chris Doehring <chrisdoehring@gmail.com>
2023-08-11 04:22:45 +0000

    Querying database on interval.

--- Models.cs
+++ Models.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Models {
 
     public class StatusData {
@@ -9,6 +11,39 @@ namespace Models {
     }
     public class StatusResponse {
         public StatusData data { get; set; }
+
+    }
+
+    public class KenwoodGpsLogRecord
+    {
+
+        public string name { get; set; }
+        public long unit_id { get; set; }
+        public string e_w { get; set; }
+        public string n_s { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public DateTime recorded_at { get; set; }
+        public decimal latitude { get; set; }
+        public decimal longitude { get; set; }
+        public int system_id { get; set; }
+    }
+
+    public class EarthRangerLocation
+    {
+        public decimal lon { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class EarthRangerObservation
+    {
+        public EarthRangerLocation location { get; set; }
+        public DateTime recorded_at { get; set; }
+        public string manufacturer_id { get; set; }
+        public string subject_name { get; set; }
+        public IDictionary<string, object> additional { get; set; }
+
+
 
     }
 }
