9f64fa5ed4da69c906a06801e53a7b91f2741f2e
Chris Doehring <chrisdoehring@gmail.com>
2023-08-29 00:22:25 +0000

    Rearrange files to include multiple projects.

---
+++ unittests/GpsLogTest.cs
@@ -0,0 +1,15 @@
+
+
+namespace kenwood_unittests
+{
+    public class GpsLogTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            DataPump pump = new DataPump();
+            Assert.True(true);
+
+        }
+    }
+}
